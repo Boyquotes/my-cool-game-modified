fa757363646247ae5551c1a8ccce445e8b1832fc
reubenmanning <reubenmanning1@gmail.com>
2023-02-21 23:10:10 +0200

    Camera movement was tied to fps. Resolved.
    Still experiencing jitter in high refresh rate monitors. Moved move_and_slide to _process for now unless issues arise later.

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -12,15 +12,17 @@ enum PlayerState { IDLE, WALKING, RUNNING, STOPPING, JUMPING, FALLING, AIMING, S
 @export var run_multiplier: float
 @export var gravity_acceleration: float
 @export var turn_speed: float
+@export var follow_camera_speed: float
 
-var input_vector: Vector2
 var h_camera_rotation: Vector3
 var v_camera_rotation: Vector3
 var movement_state: int
+var direction: Vector3
 
 @onready var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var h_cam: Node3D = $CameraRig/HCam
 @onready var v_cam: Node3D = $CameraRig/HCam/VCam
+@onready var cam: Node3D = $CameraRig
 @onready var collider: CollisionShape3D = $Collider
 
 func _ready():
@@ -40,18 +42,22 @@ func _input(event):
 	if event.is_action_pressed("run"):
 		movement_state = PlayerState.RUNNING
 
-
 #Logic for unhandled events
-func _unhandled_input(event):
+func _unhandled_input(event: InputEvent) -> void:
 	if (event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED):
-		#store input values for camera rotation
-		input_vector = -event.relative * mouse_sensitivity
+		h_cam.rotation.y -= event.relative.x * 0.01
+		v_cam.rotation.x -= event.relative.y * 0.01
+		v_cam.rotation.x = clamp(v_cam.rotation.x, deg_to_rad(min_camera_angle), deg_to_rad(max_camera_angle))
 
-func _process(delta):
-	rotate_camera(delta)
-
-func _physics_process(delta):
+func _process(delta: float) -> void:
 	player_movement(delta)
+	
+	#rotate the character collider to face the direction of the user input
+	if (velocity != Vector3.ZERO):
+		collider.rotation.y = lerp_angle(collider.rotation.y, atan2(-direction.x, -direction.z), turn_speed * delta)
+	
+func _physics_process(_delta: float) -> void:
+	pass
 
 ## Calculates player movement and moves the character body
 func player_movement(delta: float) -> void:
@@ -65,7 +71,7 @@ func player_movement(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	var direction = (h_cam.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	direction = (h_cam.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	#sprinting and walking speeds
 	if (direction != Vector3.ZERO and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED):
@@ -80,25 +86,7 @@ func player_movement(delta: float) -> void:
 		movement_state = PlayerState.IDLE
 		velocity.x = move_toward(velocity.x, 0, speed)
 		velocity.z = move_toward(velocity.z, 0, speed)
-		
-	#rotate the character collider to face the direction of the user input
-	if (velocity != Vector3.ZERO):
-		collider.rotation.y = lerp_angle(collider.rotation.y, atan2(-direction.x, -direction.z), turn_speed * delta)
-		
+	
 	move_and_slide()
-
-## Rotates the camera rig attached to the player based on user input
-func rotate_camera(delta: float) -> void:
-	#Rotate the camera horizontally
-	h_camera_rotation.y += input_vector.x * delta
-	h_cam.transform.basis = Basis.from_euler(h_camera_rotation)
-	
-	#Rotate the camera vertically
-	v_camera_rotation.x += input_vector.y * delta
-	v_camera_rotation.x = clamp(v_camera_rotation.x, deg_to_rad(min_camera_angle), deg_to_rad(max_camera_angle))
-	v_cam.transform.basis = Basis.from_euler(v_camera_rotation)
-	
-	#reset input vector to stop camera from spinning constantly
-	input_vector = Vector2.ZERO
 	
 
